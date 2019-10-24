7a92560f1de6c6ae15b537124fd4ce45059db4fb
zuriros <hanami33rosas@gmail.com>
2019-10-24 00:08:04 -0500

    primera practica de js terminada

---
+++ JavaScript Drum Kit/doc.js
@@ -0,0 +1,24 @@
+
+const playSound = (e) => {    
+  const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing'); 
+}
+
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;   
+  const keys = document.querySelectorAll('.key');
+  e.target.classList.remove('playing');  
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition)
+});
+
+window.addEventListener('keydown', playSound);
+
+
