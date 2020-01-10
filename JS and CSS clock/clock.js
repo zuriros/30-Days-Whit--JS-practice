bde1a31d969a5ed32164a8ed81eddb8da232f5dc
zuriros <hanami33rosas@gmail.com>
2020-01-09 23:37:18 -0500

    repasando código y terminando el día 4

--- JS and CSS clock/clock.js
+++ JS and CSS clock/clock.js
@@ -5,15 +5,22 @@ const hourHand = document.querySelector('.hour-hand');
 const setDate = () => {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds/60) * 360) + 90;   
+ // calculamos los grados que a de desplazarse secondHand
+  const secondsDegrees = ((seconds/60) * 360) + 90;// se agrega 90 para que sea el punto de inicio  
+// se agrega a estilos los grados a desplazarse por cada segundo
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
    
+   
   const mins = now.getMinutes();
+  // calculamos los grados que a de desplazarse mindHand
   const minsDegrees = ((mins/60) * 360) + 90;
+  // se agrega a estilos los grados a desplazarse por cada minuto
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hours = now.getHours();  
-  const hoursDegrees = (hours/12) * 360 + 90;
+  // calculamos los grados que a de desplazarse hourHand
+  const hoursDegrees = ((hours/12) * 360) + 90;
+  // se agrega a estilos los grados a desplazarse por cada hora
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 };
--- JavaScript Drum Kit/doc.js
+++ JavaScript Drum Kit/doc.js
@@ -1,21 +1,28 @@
 
-const playSound = (e) => {    
+const playSound = (e) => {        
   const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
   const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
+  //si el audio es null, paramos el recorrido de la función
   if (!audio) return;
+  //si el audio existe, reproducimos el sonido con un tiempo de cero para que sea al instante
   audio.currentTime = 0;
   audio.play();
+  //agregamos un cambio de estilos cada vez que precionemos alguna tecla. 
   key.classList.add('playing'); 
 }
 
+//creamos una función para remover el estilo agregado por presionar alguna tecla existente
 const removeTransition = (e) => {
-  if (e.propertyName !== 'transform') return;   
-  const keys = document.querySelectorAll('.key');
-  e.target.classList.remove('playing');  
+  // agregamos la condicional donde nos aseguramos que el evento sea de transform
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing'); 
+  console.log(e.target.classList);
 };
-
+//Llamamos a todo los elementos de clase key
 const keys = document.querySelectorAll('.key');
+//recorremos cada clase
 keys.forEach((key) => {
+  //al escuchar el evento de transición aplicamos la función de remover estilos
   key.addEventListener('transitionend', removeTransition)
 });
 
